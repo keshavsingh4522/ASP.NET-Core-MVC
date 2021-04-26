be109714afef633bbde5b349370976cb4e9765b4
Keshav Singh <keshavsingh4522@gmail.com>
2021-04-26 14:44:58 +0530

    razor base class and partial class

---
+++ Notes/Blazor/Base Class/CounterBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        // change private to protected or public
+        protected int currentCount = 0;
+        // one way binding
+        protected string fontsFamily = "Sitka Display";
+        [Parameter]
+        public int Incr { get; set; } = 1;
+        protected void IncrementCount()
+        {
+            currentCount += Incr;
+        }
+    }
+}
